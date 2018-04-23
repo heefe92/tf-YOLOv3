f624f88ff2500651b07016878644bc3fa6e2ad08
heecheol <heefe92@naver.com>
2018-04-23 21:33:17 +0900

    Add Loss Function and Training Function ( Learning using pre-training weights , batch norm does not learn because batch size is small.. )

--- Detector.py
+++ Detector.py
@@ -2,14 +2,12 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import utils
+import BatchGenerator
 import time
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
 class Detector():
     def __init__(self,sess= tf.InteractiveSession(),isRestore=False):
-        self.sess=sess
-
-        self.input_img = tf.placeholder(tf.float32, shape=(None, 416, 416, 3))
         # set some parameters
         self.net_h, self.net_w = 416, 416
         self.obj_thresh, self.nms_thresh = 0.5, 0.45
@@ -24,13 +22,76 @@ class Detector():
                   "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", \
                   "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", \
                   "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+        self.batch_size=2
+        self.min_net_size = 320
+        self.max_net_size = 608
+        self.ignore_thresh = 0.5
+        self.warmup_batches = 3
+        self.learning_rate=0.00001
 
-        self.logits=self.make_yolov3_model(self.input_img)
+        #self.anchors = tf.constant(anchors, dtype='float', shape=[3,1,1,1,3,2])
 
-        tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='yolo3/conv_block')).restore(
-            self.sess, "./Weights/Yolov3.ckpt")
-        print('success key model initialize')
-    def _conv_block(self,inp, convs, skip=True,trainable=False):
+        cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(self.max_net_size), [self.max_net_size]), (1, self.max_net_size, self.max_net_size, 1, 1)))
+        cell_y = tf.transpose(cell_x, (0, 2, 1, 3, 4))
+        self.cell_grid = tf.tile(tf.concat([cell_x, cell_y], -1), [self.batch_size, 1, 1, 3, 1])
+
+
+        self.sess=sess
+
+        self.input_img = tf.placeholder(tf.float32, shape=(None, None, None, 3))
+        self.ground_truth = [tf.placeholder(tf.float32, shape=(None, None, None,3, len(self.labels)+5)),
+                             tf.placeholder(tf.float32, shape=(None, None, None,3, len(self.labels)+5)),
+                             tf.placeholder(tf.float32, shape=(None, None, None,3, len(self.labels)+5))]
+        self.ground_truth_boxes = tf.placeholder(tf.float32, shape=(None, 1, 1, 1, None, 4))
+
+        self.logits = self.make_yolov3_model(self.input_img)
+
+        self.loss = tf.reduce_sum(self.LossFunction(self.input_img, self.logits, self.ground_truth, self.ground_truth_boxes))
+        self.sqrt_loss = tf.sqrt(self.loss)
+
+
+        train_ints, valid_ints, labels = utils.create_training_instances(
+            'F:/DataSet/COCO/Annotations/instances_train2017.json',
+            'F:/DataSet/COCO/train/',
+            'coco_train_data',
+            'F:/DataSet/COCO/Annotations/instances_val2017.json',
+            'F:/DataSet/COCO/val/',
+            'coco_val_data',
+            self.labels
+        )
+        self.trainBatchGenerator = BatchGenerator.BatchGenerator(
+            train_ints, self.anchors, self.labels,
+            shuffle = True,
+            jitter = 0.3,
+            norm = utils.normalize,
+            batch_size=self.batch_size
+        )
+        self.validBatchGenerator = BatchGenerator.BatchGenerator(
+            valid_ints, self.anchors, self.labels,
+            shuffle = True,
+            jitter = 0.3,
+            norm = utils.normalize,
+            batch_size=self.batch_size)
+        tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='yolo3/conv_block')).restore(self.sess, "./Weights/Yolov3.ckpt")
+
+        self.train_op=tf.train.AdamOptimizer(self.learning_rate).minimize(self.sqrt_loss)
+
+        # List all global variables
+        global_vars = tf.global_variables()
+
+        # Find initialized status for all variables
+        is_var_init = [tf.is_variable_initialized(var) for var in global_vars]
+        is_initialized = self.sess.run(is_var_init)
+
+        # List all variables that were not initialized previously
+        not_initialized_vars = [var for (var, init) in
+                                zip(global_vars, is_initialized) if not init]
+
+        # Initialize all uninitialized variables found, if any
+        if len(not_initialized_vars):
+            self.sess.run(tf.variables_initializer(not_initialized_vars))
+        print('success model initialize')
+    def _conv_block(self,inp, convs, skip=True,trainable=True):
         with tf.variable_scope('conv_block'):
             x = inp
             count = 0
@@ -50,10 +111,10 @@ class Detector():
                                    use_bias=False if conv['bnorm'] else True,
                                    trainable=trainable)
                 if conv['bnorm']:
-                    gamma=tf.get_variable(name='bnorm_'+str(conv['layer_idx'])+'/gamma',shape=(conv['filter']),trainable=trainable)
-                    beta = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/beta', shape=(conv['filter']),trainable=trainable)
-                    mean = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/moving_mean', shape=(conv['filter']),trainable=trainable)
-                    variance = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/moving_variance', shape=(conv['filter']),trainable=trainable)
+                    gamma=tf.get_variable(name='bnorm_'+str(conv['layer_idx'])+'/gamma',shape=(conv['filter']),trainable=False)
+                    beta = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/beta', shape=(conv['filter']),trainable=False)
+                    mean = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/moving_mean', shape=(conv['filter']),trainable=False)
+                    variance = tf.get_variable(name='bnorm_' + str(conv['layer_idx']) + '/moving_variance', shape=(conv['filter']),trainable=False)
                     x = tf.nn.batch_normalization(x=x,mean=mean,variance=variance,scale=gamma,offset=beta,variance_epsilon=0.001)
                 if conv['leaky']:
                     x = tf.nn.leaky_relu(x,alpha=0.1, name='leaky_' + str(conv['layer_idx']))
@@ -122,7 +183,7 @@ class Detector():
             # Layer 80 => 82
             yolo_82=self._conv_block(x, [
                 {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 80},
-                {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
+                {'filter': 3*(len(self.labels)+5), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
                  'layer_idx': 81}], skip=False)
             # Layer 83 => 86
             x = self._conv_block(x, [
@@ -142,7 +203,7 @@ class Detector():
             yolo_94=self._conv_block(x,
                                             [{'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True,
                                               'layer_idx': 92},
-                                             {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
+                                             {'filter': 3*(len(self.labels)+5), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
                                               'layer_idx': 93}], skip=False)
             # Layer 95 => 98
             x = self._conv_block(x, [
@@ -163,10 +224,167 @@ class Detector():
                  'layer_idx': 103},
                 {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True,
                  'layer_idx': 104},
-                {'filter': 255, 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
+                {'filter': 3*(len(self.labels)+5), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
                  'layer_idx': 105}], skip=False)
 
         return [yolo_82,yolo_94,yolo_106]
+    def LossFunction(self, input_image, y_preds, y_trues, true_boxes):
+        result_loss=[]
+        for y_idx,y_pred in enumerate(y_preds):
+            # adjust the shape of the y_predict [batch, grid_h, grid_w, 3, 4+1+nb_class]
+            y_pred = tf.reshape(y_pred, tf.concat([tf.shape(y_pred)[:3], tf.constant([3, -1])], axis=0))
+            # initialize the masks
+            object_mask = tf.expand_dims(y_trues[y_idx][..., 4], 4)
+            no_object_mask = 1 - object_mask
+
+            # the variable to keep track of number of batches processed
+            batch_seen = tf.Variable(0.)
+
+            # compute grid factor and net factor
+            grid_h = tf.shape(y_trues[y_idx])[1]
+            grid_w = tf.shape(y_trues[y_idx])[2]
+            grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 2])
+
+            net_h = tf.shape(input_image)[1]
+            net_w = tf.shape(input_image)[2]
+            net_factor = tf.reshape(tf.cast([net_w, net_h], tf.float32), [1, 1, 1, 1, 2])
+
+            """
+            Adjust prediction
+            """
+            pred_box_xy = (self.cell_grid[:, :grid_h, :grid_w, :, :] + tf.sigmoid(y_pred[..., :2]))  # sigma(t_xy) + c_xy
+            pred_box_wh = y_pred[..., 2:4]  # t_wh
+            pred_box_conf = tf.expand_dims(tf.sigmoid(y_pred[..., 4]), 4)  # adjust confidence
+            pred_box_class = tf.sigmoid(y_pred[..., 5:])  # adjust class probabilities
+
+            """
+            Adjust ground truth
+            """
+            true_box_xy = y_trues[y_idx][..., 0:2]  # (sigma(t_xy) + c_xy)
+            true_box_wh = y_trues[y_idx][..., 2:4]  # t_wh
+            true_box_conf = tf.expand_dims(y_trues[y_idx][..., 4], 4)
+            true_box_class = y_trues[y_idx][..., 5:]
+
+            """
+            Compare each predicted box to all true boxes
+            """
+            # initially, drag all objectness of all boxes to 0
+            conf_delta = pred_box_conf - 0
+
+            # then, ignore the boxes which have good overlap with some true box
+            true_xy = true_boxes[..., 0:2] / grid_factor
+            true_wh = true_boxes[..., 2:4] / net_factor
+
+            true_wh_half = true_wh / 2.
+            true_mins = true_xy - true_wh_half
+            true_maxes = true_xy + true_wh_half
+
+            pred_xy = tf.expand_dims(pred_box_xy / grid_factor, 4)
+            pred_wh = tf.expand_dims(tf.exp(pred_box_wh) * np.reshape(self.anchors[y_idx],[1,1,1,3,2]) / net_factor, 4)
+
+            pred_wh_half = pred_wh / 2.
+            pred_mins = pred_xy - pred_wh_half
+            pred_maxes = pred_xy + pred_wh_half
+
+            intersect_mins = tf.maximum(pred_mins, true_mins)
+            intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+            intersect_wh = tf.maximum(intersect_maxes - intersect_mins, 0.)
+            intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+
+            true_areas = true_wh[..., 0] * true_wh[..., 1]
+            pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+
+            union_areas = pred_areas + true_areas - intersect_areas
+            iou_scores = tf.truediv(intersect_areas, union_areas)
+
+            best_ious = tf.reduce_max(iou_scores, axis=4)
+            conf_delta *= tf.expand_dims(tf.to_float(best_ious < self.ignore_thresh), 4)
+
+            """
+            Compute some online statistics
+            """
+            true_xy = true_box_xy / grid_factor
+            true_wh = tf.exp(true_box_wh) * np.reshape(self.anchors[y_idx],[1,1,1,3,2]) / net_factor
+
+            true_wh_half = true_wh / 2.
+            true_mins = true_xy - true_wh_half
+            true_maxes = true_xy + true_wh_half
+
+            pred_xy = pred_box_xy / grid_factor
+            pred_wh = tf.exp(pred_box_wh) * np.reshape(self.anchors[y_idx],[1,1,1,3,2]) / net_factor
+
+            pred_wh_half = pred_wh / 2.
+            pred_mins = pred_xy - pred_wh_half
+            pred_maxes = pred_xy + pred_wh_half
+
+            intersect_mins = tf.maximum(pred_mins, true_mins)
+            intersect_maxes = tf.minimum(pred_maxes, true_maxes)
+            intersect_wh = tf.maximum(intersect_maxes - intersect_mins, 0.)
+            intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
+
+            true_areas = true_wh[..., 0] * true_wh[..., 1]
+            pred_areas = pred_wh[..., 0] * pred_wh[..., 1]
+
+            union_areas = pred_areas + true_areas - intersect_areas
+            iou_scores = tf.truediv(intersect_areas, union_areas)
+            iou_scores = object_mask * tf.expand_dims(iou_scores, 4)
+
+            count = tf.reduce_sum(tf.to_float(object_mask))
+            count_noobj = tf.reduce_sum(tf.to_float(no_object_mask))
+            recall50 = tf.reduce_sum(tf.to_float(iou_scores >= 0.5)) / (count + 1e-3)
+            recall75 = tf.reduce_sum(tf.to_float(iou_scores >= 0.75)) / (count + 1e-3)
+            avg_iou = tf.reduce_sum(iou_scores) / (count + 1e-3)
+            avg_obj = tf.reduce_sum(object_mask * pred_box_conf * true_box_conf) / (count + 1e-3)
+            avg_noobj = tf.reduce_sum(no_object_mask * pred_box_conf) / (count_noobj + 1e-3)
+            avg_cat = tf.reduce_sum(object_mask * pred_box_class * true_box_class) / (count + 1e-3)
+
+            """
+            Warm-up training
+            """
+            batch_seen = tf.assign_add(batch_seen, 1.)
+
+            true_box_xy, true_box_wh, xywh_mask = tf.cond(tf.less(batch_seen, self.warmup_batches + 1),
+                                                          lambda: [true_box_xy + (
+                                                                  0.5 + self.cell_grid[:, :grid_h, :grid_w, :,
+                                                                        :]) * no_object_mask,
+                                                                   true_box_wh + tf.zeros_like(
+                                                                       true_box_wh) * no_object_mask,
+                                                                   tf.ones_like(object_mask)],
+                                                          lambda: [true_box_xy,
+                                                                   true_box_wh,
+                                                                   object_mask])
+            """
+            Compare each true box to all anchor boxes
+            """
+            xywh_scale = tf.exp(true_box_wh) * np.reshape(self.anchors[y_idx],[1,1,1,3,2]) / net_factor
+            xywh_scale = tf.expand_dims(2 - xywh_scale[..., 0] * xywh_scale[..., 1],
+                                        axis=4)  # the smaller the box, the bigger the scale
+
+            xy_delta = xywh_mask * (pred_box_xy - true_box_xy) * xywh_scale
+            wh_delta = xywh_mask * (pred_box_wh - true_box_wh) * xywh_scale
+            conf_delta = object_mask * (pred_box_conf - true_box_conf) * 5 + (1 - object_mask) * conf_delta
+            class_delta = object_mask * (pred_box_class - true_box_class)
+
+            loss = tf.reduce_sum(tf.square(xy_delta), list(range(1, 5))) + \
+                   tf.reduce_sum(tf.square(wh_delta), list(range(1, 5))) + \
+                   tf.reduce_sum(tf.square(conf_delta), list(range(1, 5))) + \
+                   tf.reduce_sum(tf.square(class_delta), list(range(1, 5)))
+
+            loss = tf.cond(tf.less(batch_seen, self.warmup_batches + 1),  # add 10 to the loss if this is the warmup stage
+                           lambda: loss + 10,
+                           lambda: loss)
+
+            # loss = tf.Print(loss, [grid_h, avg_obj], message='avg_obj \t\t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, avg_noobj], message='avg_noobj \t\t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, avg_iou], message='avg_iou \t\t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, avg_cat], message='avg_cat \t\t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, recall50], message='recall50 \t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, recall75], message='recall75 \t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, count], message='count \t', summarize=1000)
+            # loss = tf.Print(loss, [grid_h, tf.reduce_sum(loss)], message='loss: \t', summarize=1000)
+            result_loss.append(loss)
+
+        return result_loss
     def Test(self,imgPath=None,img=None):
         # preprocess the image
         image = cv2.imread(imgPath)
@@ -205,3 +423,18 @@ class Detector():
         utils.draw_boxes(image, boxes, self.labels, self.obj_thresh)
 
         return image
+    def Training(self):
+        for epoch in range(10):
+            loss_sum=0
+            for iter in range(self.trainBatchGenerator.__len__()):
+                train_batch = self.trainBatchGenerator.__getitem__(0)
+                feed_dict = {self.input_img: train_batch[0], self.ground_truth_boxes: train_batch[1],
+                             self.ground_truth[0]: train_batch[2], self.ground_truth[1]: train_batch[3],
+                             self.ground_truth[2]: train_batch[4]}
+
+                loss,_=self.sess.run([self.loss,self.train_op],feed_dict=feed_dict)
+                loss_sum+=loss
+                if (iter%100==0):
+                    print('iter = ',iter,' loss = ',loss)
+
+            tf.train.Saver().save(self.sess, "./Weights/test_"+str(epoch)+".ckpt")
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import cv2
 import numpy as np
+import copy
+import os
+import pickle
+import xml.etree.ElementTree as ET
+import json
 
 def _sigmoid(x):
     return 1. / (1. + np.exp(-x))
@@ -43,7 +48,6 @@ def decode_netout(netout, anchors, obj_thresh, nms_thresh, net_h, net_w):
             boxes.append(box)
 
     return boxes
-
 def preprocess_input(image, net_h, net_w):
     new_h, new_w, _ = image.shape
 
@@ -55,7 +59,7 @@ def preprocess_input(image, net_h, net_w):
         new_w = int((new_w * net_h) / new_h)
         new_h = int(net_h)
     # resize the image to the new size
-    resized = cv2.resize(image[:, :, ::-1] / 255., (new_w, new_h))
+    resized = cv2.resize(image / 255., (new_w, new_h))
 
     # embed the image into the standard letter box
     new_image = np.ones((net_h, net_w, 3)) * 0.5
@@ -65,9 +69,6 @@ def preprocess_input(image, net_h, net_w):
 
 
     return new_image
-
-
-
 def correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w):
     if (float(net_w) / image_w) < (float(net_h) / image_h):
         new_w = net_w
@@ -84,9 +85,6 @@ def correct_yolo_boxes(boxes, image_h, image_w, net_h, net_w):
         boxes[i].xmax = int((boxes[i].xmax - x_offset) / x_scale * image_w)
         boxes[i].ymin = int((boxes[i].ymin - y_offset) / y_scale * image_h)
         boxes[i].ymax = int((boxes[i].ymax - y_offset) / y_scale * image_h)
-
-
-
 def do_nms(boxes, nms_thresh):
     if len(boxes) > 0:
         nb_class = len(boxes[0].classes)
@@ -106,8 +104,6 @@ def do_nms(boxes, nms_thresh):
 
                 if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
                     boxes[index_j].classes[c] = 0
-
-
 def draw_boxes(image, boxes, labels, obj_thresh):
     colors = [(254.0, 254.0, 254), (239.88888888888889, 211.66666666666669, 127),
               (225.77777777777777, 169.33333333333334, 0), (211.66666666666669, 127.0, 254),
@@ -169,8 +165,6 @@ def draw_boxes(image, boxes, labels, obj_thresh):
                         1e-3 * image.shape[0],
                         colors[label], 2)
     return image
-
-
 def _interval_overlap(interval_a, interval_b):
     x1, x2 = interval_a
     x3, x4 = interval_b
@@ -223,3 +217,194 @@ class BoundBox:
 
         return self.score
 
+
+def _rand_scale(scale):
+    scale = np.random.uniform(1, scale)
+    return scale if (np.random.randint(2) == 0) else 1. / scale;
+def _constrain(min_v, max_v, value):
+    if value < min_v: return min_v
+    if value > max_v: return max_v
+    return value
+def random_flip(image, flip):
+    if flip == 1: return cv2.flip(image, 1)
+    return image
+def correct_bounding_boxes(boxes, new_w, new_h, net_w, net_h, dx, dy, flip, image_w, image_h):
+    boxes = copy.deepcopy(boxes)
+
+    # randomize boxes' order
+    np.random.shuffle(boxes)
+
+    # correct sizes and positions
+    sx, sy = float(new_w) / image_w, float(new_h) / image_h
+    zero_boxes = []
+
+    for i in range(len(boxes)):
+        boxes[i]['xmin'] = int(_constrain(0, net_w, boxes[i]['xmin'] * sx + dx))
+        boxes[i]['xmax'] = int(_constrain(0, net_w, boxes[i]['xmax'] * sx + dx))
+        boxes[i]['ymin'] = int(_constrain(0, net_h, boxes[i]['ymin'] * sy + dy))
+        boxes[i]['ymax'] = int(_constrain(0, net_h, boxes[i]['ymax'] * sy + dy))
+
+        if boxes[i]['xmax'] <= boxes[i]['xmin'] or boxes[i]['ymax'] <= boxes[i]['ymin']:
+            zero_boxes += [i]
+            continue
+
+        if flip == 1:
+            swap = boxes[i]['xmin'];
+            boxes[i]['xmin'] = net_w - boxes[i]['xmax']
+            boxes[i]['xmax'] = net_w - swap
+
+    boxes = [boxes[i] for i in range(len(boxes)) if i not in zero_boxes]
+
+    return boxes
+def random_distort_image(image, hue=18, saturation=1.5, exposure=1.5):
+    # determine scale factors
+    dhue = np.random.uniform(-hue, hue)
+    dsat = _rand_scale(saturation);
+    dexp = _rand_scale(exposure);
+
+    # convert RGB space to HSV space
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV).astype('float')
+
+    # change satuation and exposure
+    image[:, :, 1] *= dsat
+    image[:, :, 2] *= dexp
+
+    # change hue
+    image[:, :, 0] += dhue
+    image[:, :, 0] -= (image[:, :, 0] > 180) * 180
+    image[:, :, 0] += (image[:, :, 0] < 0) * 180
+
+    # convert back to RGB from HSV
+    return cv2.cvtColor(image.astype('uint8'), cv2.COLOR_HSV2RGB)
+def apply_random_scale_and_crop(image, new_w, new_h, net_w, net_h, dx, dy):
+    im_sized = cv2.resize(image, (new_w, new_h))
+
+    if dx > 0:
+        im_sized = np.pad(im_sized, ((0, 0), (dx, 0), (0, 0)), mode='constant', constant_values=127)
+    else:
+        im_sized = im_sized[:, -dx:, :]
+    if (new_w + dx) < net_w:
+        im_sized = np.pad(im_sized, ((0, 0), (0, net_w - (new_w + dx)), (0, 0)), mode='constant', constant_values=127)
+
+    if dy > 0:
+        im_sized = np.pad(im_sized, ((dy, 0), (0, 0), (0, 0)), mode='constant', constant_values=127)
+    else:
+        im_sized = im_sized[-dy:, :, :]
+
+    if (new_h + dy) < net_h:
+        im_sized = np.pad(im_sized, ((0, net_h - (new_h + dy)), (0, 0), (0, 0)), mode='constant', constant_values=127)
+
+    return im_sized[:net_h, :net_w, :]
+
+
+def parse_coco_annotation(ann_file, img_dir, cache_name, labels=[]):
+    if os.path.exists(cache_name):
+        with open(cache_name, 'rb') as handle:
+            cache = pickle.load(handle)
+        all_insts, seen_labels = cache['all_insts'], cache['seen_labels']
+
+    else:
+        all_insts = []
+        seen_labels = {}
+        ann_data=json.load(open(ann_file))
+        categories=ann_data['categories']
+        labels_id=[]
+        print(len(ann_data['annotations']))
+        for category in categories:
+            if category['name'] in labels:
+                labels_id.append(category['id'])
+        for idx,ann in enumerate(ann_data['annotations']):
+            if(idx%1000==0):
+                print(idx)
+            if ann['category_id'] in labels_id:
+                img_id=str(ann['image_id'])
+                while len(img_id)<12:
+                    img_id='0'+img_id
+                file_name=img_dir+img_id+'.jpg'
+                flag=True
+                for inst in all_insts:
+                    if(inst['filename']==file_name):
+                        object = {}
+                        object['name'] = labels[labels_id.index(ann['category_id'])]
+                        object['xmin'] = ann['bbox'][0]
+                        object['ymin'] = ann['bbox'][1]
+                        object['xmax'] = int(ann['bbox'][0]+ann['bbox'][2]) - 1
+                        object['ymax'] = int(ann['bbox'][1]+ann['bbox'][3]) - 1
+                        inst['object'].append(object)
+                        if object['name'] in seen_labels:
+                            seen_labels[object['name']] += 1
+                        else:
+                            seen_labels[object['name']] = 1
+                        flag=False
+                if flag:
+                    instance={}
+                    instance['filename'] = file_name
+                    img=cv2.imread(file_name)
+                    instance['height'] = img.shape[0]
+                    instance['width'] = img.shape[1]
+                    instance['object']=[]
+                    object={}
+
+                    object['name']=labels[labels_id.index(ann['category_id'])]
+                    object['xmin']= int(ann['bbox'][0])
+                    object['ymin'] = int(ann['bbox'][1])
+                    object['xmax'] = int(ann['bbox'][0]+ann['bbox'][2]) - 1
+                    object['ymax'] = int(ann['bbox'][1]+ann['bbox'][3]) - 1
+                    instance['object'].append(object)
+                    if object['name'] in seen_labels:
+                        seen_labels[object['name']] += 1
+                    else:
+                        seen_labels[object['name']] = 1
+                    all_insts.append(instance)
+
+                #print(str(ann['image_id']))
+
+        cache = {'all_insts': all_insts, 'seen_labels': seen_labels}
+        with open(cache_name, 'wb') as handle:
+            pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return all_insts, seen_labels
+
+def create_training_instances(
+    train_annot_folder,
+    train_image_folder,
+    train_cache,
+    valid_annot_folder,
+    valid_image_folder,
+    valid_cache,
+    labels,
+):
+    # parse annotations of the training set
+    train_ints, train_labels = parse_coco_annotation(train_annot_folder, train_image_folder, train_cache, labels)
+    # parse annotations of the validation set, if any, otherwise split the training set
+    if os.path.exists(valid_annot_folder):
+        valid_ints, valid_labels = parse_coco_annotation(valid_annot_folder, valid_image_folder, valid_cache, labels)
+    else:
+        print("valid_annot_folder not exists. Spliting the trainining set.")
+
+        train_valid_split = int(0.8*len(train_ints))
+        np.random.shuffle(train_ints)
+
+        valid_ints = train_ints[train_valid_split:]
+        train_ints = train_ints[:train_valid_split]
+
+    # compare the seen labels with the given labels in config.json
+    if len(labels) > 0:
+        overlap_labels = set(labels).intersection(set(train_labels.keys()))
+
+        print('Seen labels: \t\t'  + str(train_labels))
+        print('Given labels: \t\t' + str(labels))
+        print('Overlap labels: \t' + str(list(overlap_labels)))
+
+        # return None, None, None if some given label is not in the dataset
+        if len(overlap_labels) < len(labels):
+            print('Some labels have no annotations! Please revise the list of labels in the config.json.')
+            # return None, None, None
+    else:
+        print('No labels are provided. Train on all seen labels.')
+        print(train_labels)
+        labels = train_labels.keys()
+
+    return train_ints, valid_ints, sorted(labels)
+def normalize(image):
+    return image/255.
